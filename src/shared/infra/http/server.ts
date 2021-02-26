97726f03ff04c63e04da77dc8dcbe0040232df7c
= <=>
2021-02-26 20:28:20 -0300

    [FEAT] Treat errors in api

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,10 @@
 /* eslint-disable no-console */
 import { config } from 'dotenv';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
 config();
@@ -11,6 +13,22 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
